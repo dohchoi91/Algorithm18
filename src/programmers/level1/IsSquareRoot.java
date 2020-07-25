6b2c45c6377b85cc9cf4120b0875a4853e9e227f
dohchoi91 <dohchoi91@gmail.com>
2020-07-25 15:19:13 +0900

    제곱근 판별

---
+++ src/programmers/level1/IsSquareRoot.java
@@ -0,0 +1,26 @@
+package programmers.level1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+// 제곱근 판별
+// https://programmers.co.kr/learn/courses/30/lessons/12934
+public class IsSquareRoot {
+    public long solution(long n) {
+    	double value = Math.sqrt(n);
+    	// 소수점 판단은 엄청 단순하게 가능
+    	long answer = (value == (int)value) ? (long)Math.pow(value + 1, 2) : -1;
+        return answer;
+    }
+
+	@Test
+	void test1() {
+		assertEquals(144, solution(121));
+	}
+	
+	@Test
+	void test2() {
+		assertEquals(-1, solution(3));
+	}
+}
