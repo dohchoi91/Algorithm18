e1b4509c33a911793ff03af43e4ce5ad8ed5c4b4
dohchoi <dohchoi91@gmail.com>
2020-09-22 08:53:54 +0900

    Update DivisorSum.java
    약수의 합 (개선)

--- src/programmers/level1/DivisorSum.java
+++ src/programmers/level1/DivisorSum.java
@@ -38,7 +38,21 @@ public class DivisorSum {
 		}
 		return answer;
 	}
-
+	
+	// 20-09-22 : 굳이 map이 필요 없을 듯. 범위만 정확하게 지정해주면 끝
+    	public int solution(int n) {
+		int answer = 0;
+		for (int i = 1; i < (int)Math.sqrt(n) + 1; i++) {
+			if (n % i == 0) {
+				int divide = n / i; 
+				answer += (i  + divide);
+				if (i == divide) {
+					answer -= divide;
+				}
+			}
+		}
+		return answer;
+	}
 	@Test
 	void test1() {
 		assertEquals(28, solution(12));
