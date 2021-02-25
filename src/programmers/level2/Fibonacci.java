d5d23fed5e7a8fcbddcdd5b338d3b5a0d3e61e22
dohchoi91 <dohchoi91@gmail.com>
2021-02-25 14:10:48 +0900

    피보나치 수열 수정

--- src/programmers/level2/Fibonacci.java
+++ src/programmers/level2/Fibonacci.java
@@ -12,33 +12,36 @@ import org.junit.jupiter.api.Test;
  */
 public class Fibonacci {
     public int solution(int n) {
-		int answer = (n == 1) ? 0 : 1;
-		int prev = 0;
-		
-		for (int i = 0; i < n - 1; i++) {
-			int tmp = answer % 1234567;
-			answer = (prev % 1234567) + (answer % 1234567);
-			prev = tmp;
-		}
-		return answer % 1234567;
-	}
+        int answer = (n == 0) ? 0 : 1;
+        int prev = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int tmp = answer % 1234567;
+            answer = (prev % 1234567) + (answer % 1234567);
+            prev = tmp;
+        }
+        return answer % 1234567;
+    }
 
 
-	@Test
-	void test1() {
-		assertEquals(2, solution(3));
-	}
-	@Test
-	void test2() {
-		assertEquals(5, solution(5));
-	}
-	@Test
-	void test3() {
-		assertEquals(1, solution(2));
-	}
-	@Test
-	void test4() {
-		assertEquals(0, solution(1));
-	}
-	
+    @Test
+    void test1() {
+        assertEquals(2, solution(3));
+    }
+
+    @Test
+    void test2() {
+        assertEquals(5, solution(5));
+    }
+
+    @Test
+    void test3() {
+        assertEquals(1, solution(2));
+    }
+
+    @Test
+    void test4() {
+        assertEquals(1, solution(1));
+    }
+
 }
